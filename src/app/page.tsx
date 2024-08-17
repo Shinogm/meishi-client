111b262a7a6b47b048444f5ffd856bfbac1f7a03
Shinogm <dg047359@gmail.com>
2024-08-17 16:25:26 -0700

    feat(create landing page): :art:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,128 @@
+"use client";
+
+import { useInView } from "react-intersection-observer";
+import { SeccionVenta } from "@/components/service/sell-point-component";
+import { ContactComponent } from "@/components/sellpoint/contact-component";
+import { SellComponent } from "@/components/sellpoint/sell-component";
 import Image from "next/image";
+import { ProductPresentation } from "@/components/service/service-component";
+import { FooterComponent } from "@/components/service/footer-component";
+import { ArchivementsComponent } from "@/components/service/archivements-component";
+import { TonersComponent } from "@/components/service/tonner-component";
 
 export default function Home() {
+  const { ref: aboutRef, inView: aboutInView } = useInView({
+    triggerOnce: false,
+  });
+  const { ref: informationRef, inView: informationInView } = useInView({
+    triggerOnce: false,
+  });
+  const { ref: servicesRef, inView: servicesInView } = useInView({
+    triggerOnce: false,
+  });
+  const { ref: achievementsRef, inView: achievementsInView } = useInView({
+    triggerOnce: false,
+  });
+  const { ref: contactRef, inView: contactInView } = useInView({
+    triggerOnce: false,
+  });
+  const { ref: tonnerRef, inView: tonnerInView } = useInView({
+    triggerOnce: false,
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <main className="bg-white min-h-screen w-full">
+      <header className="bg-[#0077B6] text-white py-6 px-8 ">
+        <div className="container mx-auto flex items-center justify-between">
+          <a className="flex items-center cursor-pointer">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
+              src="/LogoGrupoCom.webp"
               width={100}
-              height={24}
-              priority
+              height={100}
+              alt="GrupoCom Logo"
+              className="h-10 mr-4 transition-transform duration-500 hover:rotate-12"
             />
+            <h1 className="text-2xl font-bold text-black/50">GrupoCom</h1>
           </a>
+          <nav className="hidden md:flex space-x-6">
+            <div className="flex justify-center space-x-4">
+              <button
+                onClick={() =>
+                  (window.location.href =
+                    "https://api.whatsapp.com/send?phone=5216692071277&text=Hola!%20Me%20interesa%20uno%20de%20sus%20servicios")
+                }
+                className="bg-[#FFCC00] text-[#0077B6] font-bold py-2 px-4 rounded hover:bg-[#FFDD33] transition-transform duration-500 hover:scale-105"
+              >
+                Enviar Mensaje
+              </button>
+            </div>
+          </nav>
         </div>
+      </header>
+
+      <div
+        ref={contactRef}
+        className={`${
+          contactInView
+            ? "opacity-100 translate-x-0"
+            : "opacity-0 translate-x-full"
+        } transition-all duration-700 z-50 fixed top-0 right-0`}
+      >
+        <ContactComponent />
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div
+        ref={informationRef}
+        className={`${
+          informationInView
+            ? "opacity-100 translate-x-0"
+            : "opacity-0 -translate-x-full"
+        } transition-all duration-700`}
+      >
+        <SellComponent />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div
+        ref={aboutRef}
+        className={`${
+          aboutInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+        } transition-all duration-700`}
+      >
+        <SeccionVenta />
       </div>
+
+      <div
+        ref={tonnerRef}
+        className={`${
+          tonnerInView
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 translate-y-10"
+        } transition-all duration-700`}
+      >
+        <TonersComponent />
+      </div>
+
+      <div
+        ref={servicesRef}
+        className={`${
+          servicesInView ? "opacity-100 scale-100" : "opacity-0 scale-75"
+        } transition-all duration-700`}
+      >
+        <ProductPresentation id="presentation" />
+      </div>
+
+      <div
+        ref={achievementsRef}
+        className={`${
+          achievementsInView
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 translate-y-10"
+        } transition-all duration-700`}
+      >
+        <ArchivementsComponent id="achievements" />
+      </div>
+
+      <FooterComponent />
     </main>
   );
 }
