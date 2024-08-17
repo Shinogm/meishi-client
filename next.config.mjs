f042e0b21a0c0a31431074d5c8303079904d74ed
Shinogm <dg047359@gmail.com>
2024-08-17 16:21:17 -0700

    feat(principal configuration): :zap:

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,42 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'upload.wikimedia.org',
+              pathname: '/**',
+          },
+      ],
+  },
+  async redirects() {
+      return [
+        {
+          source: '/meishi',
+          destination: 'https://meishi.com.mx/',
+          permanent: false,
+          basePath: false
+        },
+        {
+          source: '/grupocom',
+          destination: 'https://grupocom.com.mx/',
+          permanent: false,
+          basePath: false
+        },
+        {
+          source: '/crt-c',
+          destination: 'https://ctrl-c.com.mx/',
+          permanent: false,
+          basePath: false
+        },
+        {
+          source: '/whatsapp',
+          destination: 'https://wa.me/551199123456',
+          permanent: false,
+          basePath: false
+        }
+      ];
+    },
+};
 
 export default nextConfig;
